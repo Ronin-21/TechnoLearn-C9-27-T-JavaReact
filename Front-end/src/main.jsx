f54806a7c4784df6d04c8a95046ae024fedf9182
matias199130 <matiasromero20121991@gmail.com>
2023-02-09 00:56:05 -0300

    a

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import './styles/index.css';
+import './styles/reset.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
