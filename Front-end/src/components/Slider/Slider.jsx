56c0ad9969b8bb1e99b619c2ea8a4e7a940c3c54
Abel Acuña <ale_lasarte@hotmail.com>
2023-02-11 00:18:20 -0300

    Fix: agregado funcionalidad al slider

--- Front-end/src/components/Slider/Slider.jsx
+++ Front-end/src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaAngleRight } from 'react-icons/fa';
 import { FaAngleLeft } from 'react-icons/fa';
 import './Slider.css';
@@ -13,12 +13,37 @@ const Slider = ({ sliderData }) => {
 	const handleNextSlide = () =>
 		setCurrentImage(currentImage === lenght - 1 ? 0 : currentImage + 1);
 
+	useEffect(() => {
+		const autoSlide = setInterval(() => {
+			handleNextSlide();
+		}, 3000);
+
+		return () => clearInterval(autoSlide);
+	}, [currentImage]);
+
+	const handleSliderMark = (index) => setCurrentImage(index);
+
 	return (
 		<div className='slider-container'>
 			<div className='slider-btn prev' onClick={handlePrevSlide}>
 				<FaAngleLeft />
 			</div>
-			<img src={sliderData[currentImage].img} alt={sliderData[currentImage].alt} />
+			<div className='slider-img-container'>
+				<img
+					src={sliderData[currentImage].img}
+					alt={sliderData[currentImage].alt}
+				/>
+			</div>
+			<div className='slider-marks-container'>
+				{sliderData.map((item, index) => {
+					return (
+						<div
+							key={item.key}
+							className={currentImage === index ? 'slider-mark active' : 'slider-mark'}
+							onClick={() => handleSliderMark(index)}></div>
+					);
+				})}
+			</div>
 			<div className='slider-btn next' onClick={handleNextSlide}>
 				<FaAngleRight />
 			</div>
--- Front-end/src/utils/bannerImages.js
+++ Front-end/src/utils/bannerImages.js
@@ -5,14 +5,17 @@ import banner3 from "../assets/img/banner3.png"
 
 export const bannerImages = [
     {
+        key: 1,
         img: banner1,
         alt: "banner1",
     },
     {
+        key: 2,
         img: banner2,
         alt: "banner2",
     },
     {
+        key: 3,
         img: banner3,
         alt: "banner3",
     },
