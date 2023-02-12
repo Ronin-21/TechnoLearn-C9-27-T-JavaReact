6293952bf804a4ab6a33ccb468387705a3005a73
Abel Acuña <ale_lasarte@hotmail.com>
2023-02-12 05:01:12 -0300

    Probando conexiones con la API

--- Front-end/src/components/Cards/CardComponent.jsx
+++ Front-end/src/components/Cards/CardComponent.jsx
@@ -3,34 +3,24 @@ import imagen from './banner3.png';
 import { BsFillPlayCircleFill } from 'react-icons/bs';
 import './Card.css';
 
-const CardComponent = () => {
+const CardComponent = ({ nombreCurso, miniaturaCurso }) => {
 	return (
 		<div>
 			{/* <--Grid--> */}
 			<div>
 				{/* Card */}
-				<div className=' card'>
-					<div className='bg-slate-300 flex flex-col'>
+				<div className='card'>
+					<div className='bg-slate-300 flex flex-col card-container'>
 						<div className='rounded-xl overflow-hidden relative'>
 							<BsFillPlayCircleFill className='btn-play' />
-							<img src={imagen} alt='' />
+							<img src={miniaturaCurso} alt='' />
 						</div>
-						<h5 className='text-2xl md:text-3xl font-medium text-center mt-3'>
-							Camp Batu Gede
-						</h5>
+						<h5 className='font-bold text-lg text-center mt-3'>{nombreCurso}</h5>
 						<p className='text-slate-500 text-lg text-center mt-3'>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, beatae.
 						</p>
 						<div className='columns-auto flex items-center justify-center gap-4  '>
 							<p className='text-slate-900 text-lg m-2 p-5'>350</p>
-							{/* <div>
-								<a
-									href='#'
-									className='text-center bg-blue-400 text-blue-700 py-2 rounded-xs '>
-									a lista de deseados
-								</a>
-							</div> */}
-
 							<button className='bg-blue-400 text-blue-700 p-2 rounded-xs h-10'>
 								Comprar
 							</button>
--- Front-end/src/pages/Home.jsx
+++ Front-end/src/pages/Home.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import Slider from '../components/Slider/Slider';
 import { bannerImages } from '../utils/bannerImages';
-import CardsCourses from '../components/CardsCourses';
 import CardComponent from '../components/Cards/CardComponent';
 import { Newsletter } from '../components/Newsletter';
+import { useFetch } from '../hook/useFetch';
 
 const Home = () => {
+	const { data, loading, error } = useFetch(
+		'https://technolearn-c9-27-t-javareact-preproduction.up.railway.app/api/cursos/list'
+	);
+
+	// const { data: data2 } = useFetch(
+	// 	'https://technolearn-c9-27-t-javareact-preproduction.up.railway.app/api/cursos/2'
+	// );
+
+	const { data: data3 } = useFetch(
+		'https://technolearn-c9-27-t-javareact-preproduction.up.railway.app/api/todos'
+	);
+
+	console.log(data3);
+
 	return (
 		<>
 			<Slider sliderData={bannerImages} />
-			{/* 	<CardsCourses/> */}
 			<div className='flex item-center justify-center min-h-screen container mx-auto p-3'>
 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-					<CardComponent />
-					<CardComponent />
-					<CardComponent />
-					<CardComponent />
+					{data.map((e, index) => {
+						return (
+							<CardComponent
+								key={e.id}
+								nombreCurso={e.nombreCurso}
+								miniaturaCurso={e.miniaturaCurso}
+							/>
+						);
+					})}
 				</div>
+				{/* <div>
+					<CardComponent
+						nombreCurso={data2.nombreCurso}
+						miniaturaCurso={data2.miniaturaCurso}
+					/>
+				</div> */}
 			</div>
 			<Newsletter />
 		</>
