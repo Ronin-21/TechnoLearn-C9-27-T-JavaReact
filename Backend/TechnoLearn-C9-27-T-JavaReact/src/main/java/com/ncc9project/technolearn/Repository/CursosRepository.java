a63884acb67bebc76685ce9f1cedcd53d66491a7
lnxxxxxxx <74507760+lnxxxxxxxx@users.noreply.github.com>
2023-02-14 21:23:54 -0300

    Revert "Develop"

--- Backend/TechnoLearn-C9-27-T-JavaReact/src/main/java/com/ncc9project/technolearn/Repository/CursosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ncc9project.technolearn.Repository;
-
-import com.ncc9project.technolearn.Model.Cursos;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CursosRepository extends CrudRepository<Cursos, Long> {
-}
