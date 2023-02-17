cd73574a118ca44d5a0f5248195bd2cc420be1d2
matias199130 <matiasromero20121991@gmail.com>
2023-02-17 00:38:14 -0300

    ia no aguanto ia no aguanto!

--- Front-end/src/layout/MainLayout.jsx
+++ Front-end/src/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/NavBar/NavBar';
+import Navbar from '../components/navBar/navBar';
 import Footer from '../components/Footer/Footer';
 
 const MainLayout = () => {
