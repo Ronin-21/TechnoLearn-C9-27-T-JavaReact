2a1887ac0b70b82a42c7e562ea74ac963a863331
matias199130 <matiasromero20121991@gmail.com>
2023-02-22 20:07:01 -0300

    sigo de prueba

--- Front-end/src/components/Cards/CardComponent.jsx
+++ Front-end/src/components/Cards/CardComponent.jsx
@@ -1,103 +1,64 @@
-import React from 'react';
-import { BsFillPlayCircleFill, BsHeart } from 'react-icons/bs';
-import { FaStar, FaRegStar, FaAngleRight } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Button from '../Button/Button';
+import React from "react";
+import { BsFillPlayCircleFill, BsHeart } from "react-icons/bs";
+import { FaStar, FaRegStar, FaAngleRight } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 import {
-	useGetUsersQuery,
-	usePutCoursesUserMutation,
-} from '../../store/api/apiSlice';
-import './Card.css';
-
-
+  useGetUsersQuery,
+  usePutCoursesUserMutation,
+} from "../../store/api/apiSlice";
+import "./Card.css";
 
 const CardComponent = ({ nombreCurso, miniaturaCurso, id }) => {
-	//...........
-	const userEmail = useSelector((state) => state.auth.userEmail);
-	const { data } = useGetUsersQuery();
-	const usuarioDataId = data?.usuarios
-		.filter((e) => e.email === userEmail)
-		.map((e) => e.id)
-		.join();
-	// console.log(usuarioDataId);
-	// console.log(data);
+  //...........
+  const userEmail = useSelector((state) => state.auth.userEmail);
+  const { data } = useGetUsersQuery();
+  const usuarioDataId = data?.usuarios
+    .filter((e) => e.email === userEmail)
+    .map((e) => e.id)
+    .join();
 
-	const [putCoursesUser, { isError, error }] = usePutCoursesUserMutation();
-	const putCurso = { idUsuario: parseInt(usuarioDataId), idCurso: id };
+  // console.log(data);
 
-	return (
-		<div className='card-container'>
-			<div className='card-img-container'>
-				<BsFillPlayCircleFill className='btn-play' />
-				<img src={miniaturaCurso} alt='' />
-			</div>
-			<h5 className='card-title'>{nombreCurso}</h5>
-			<p className='card-text'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, beatae.
-			</p>
-			<div className='card-btn-container'>
-				<div className='card-btn-fav'>
-					<div className='card-btn-stars'>
-						<FaStar />
-						<FaStar />
-						<FaStar />
-						<FaStar />
-						<FaRegStar />
-					</div>
-					<div className='flex flex-col items-center text-center font-bold'>
-						<BsHeart className='card-btn-heart' />
-						<p>Agregar a tu lista de favoritos</p>
-					</div>
-				</div>
-				<div className='card-btn-text'>
-					<p>CURSO FREE</p>
-					<Button fontSize={'2xl'} padX={4} padY={2}>
-						<Link to={`/courses/${id}`} className='flex items-center gap-3'>
-							Ir al curso <FaAngleRight />
-						</Link>
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
-	}
-/* const CardComponent = ({ nombreCurso, miniaturaCurso, id }) => {
-	return (
-		<div className='card-container'>
-			<div className='card-img-container'>
-				<BsFillPlayCircleFill className='btn-play' />
-				<img src={miniaturaCurso} alt='' />
-			</div>
-			<h5 className='card-title'>{nombreCurso}</h5>
-			<p className='card-text'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, beatae.
-			</p>
-			<div className='card-btn-container'>
-				<div className='card-btn-fav'>
-					<div className='card-btn-stars'>
-						<FaStar />
-						<FaStar />
-						<FaStar />
-						<FaStar />
-						<FaRegStar />
-					</div>
-					<div className='flex flex-col items-center text-center font-bold'>
-						<BsHeart className='card-btn-heart' />
-						<p>Agregar a tu lista de favoritos</p>
-					</div>
-				</div>
-				<div className='card-btn-text'>
-					<p>CURSO FREE</p>
-					<Button fontSize={'2xl'} padX={4} padY={2}>
-						<Link to={`/courses/${id}`} className='flex items-center gap-3'>
-							Ir al curso <FaAngleRight />
-						</Link>
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+  const [putCoursesUser, { isError, error }] = usePutCoursesUserMutation();
+  const putCurso = { idUsuario: parseInt(usuarioDataId), idCurso: id };
+
+  return (
+    <div className="card-container">
+      <div className="card-img-container">
+        <BsFillPlayCircleFill className="btn-play" />
+        <img src={miniaturaCurso} alt="" />
+      </div>
+      <h5 className="card-title">{nombreCurso}</h5>
+      <p className="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, beatae.
+      </p>
+      <div className="card-btn-container">
+        <div className="card-btn-fav">
+          <div className="card-btn-stars">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+          </div>
+          <div className="flex flex-col items-center text-center font-bold">
+            <BsHeart className="card-btn-heart" />
+            <p>Agregar a tu lista de favoritos</p>
+          </div>
+        </div>
+        <div className="card-btn-text">
+          <p>CURSO FREE</p>
+          <Button fontSize={"2xl"} padX={4} padY={2}>
+            <Link to={`/courses/${id}`} className="flex items-center gap-3">
+              Ir al curso <FaAngleRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
- */
+
 export default CardComponent;
--- Front-end/src/pages/ProfileUser.jsx
+++ Front-end/src/pages/ProfileUser.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Loading from "../components/loading/loading";
 import { useGetUsersQuery } from "../store/api/apiSlice";
+import { FaStar, FaRegStar, FaAngleRight } from "react-icons/fa";
 import CardComponent from "../components/Cards/CardComponent";
 import "../styles/profileUser.css";
+import { BsFillPlayCircleFill, BsHeart } from "react-icons/bs";
+import Button from "../components/Button/Button";
 
 const ProfileUser = () => {
   const userEmail = useSelector((state) => state.auth.userEmail);
@@ -13,22 +16,25 @@ const ProfileUser = () => {
   const idPerfilUsuario = Math.floor(Math.random() * 100);
   const isLogged = useSelector((state) => state.auth.isLoggedIn);
   const navigate = useNavigate();
-
+  
+  console.log(data.usuarios[0].cursosUsuario[0]);
   useEffect(() => {
     if (!isLogged) {
       return navigate("/login");
     }
   }, []);
 
-  console.log(usuarioData);
+  /* console.log(usuarioData);
+  console.log(usuarioData[0].nombre) */
+  /* console.log(data.usuarios[0].nombre); */
+  
   if (isLoading) return <Loading />;
   else if (isError) return <div>{error.message}</div>;
-
   return (
     <div className="profile-container">
       <div className="title-welcome">
         <h2>
-          BIENVENIDO <p>{usuarioData.nombre}</p>
+          BIENVENIDO <p>{usuarioData[0].nombre}</p>
         </h2>
       </div>
       <div className="deatils-profile">
@@ -41,7 +47,8 @@ const ProfileUser = () => {
             />
           </div>
           <div className="profile-details pt-6">
-            <p>{usuarioData.email}</p>
+          <p>email:</p>
+            <p>{usuarioData[0].email}</p>
           </div>
         </div>
       </div>
@@ -50,10 +57,39 @@ const ProfileUser = () => {
           <h3>Tus cursos</h3>
         </div>
         <div className="list-courses">
-          {/* <CardComponent
-            nombreCurso={usuarioData[0].cursosUsuario[0].nombreCurso}
-            miniaturaCurso={usuarioData[0].cursosUsuario[0].miniaturaCurso}
-          /> */}
+        <div className="card-container">
+      <div className="card-img-container">
+        <BsFillPlayCircleFill className="btn-play" />
+        <img src={data.usuarios[0].cursosUsuario[0].miniaturaCurso} alt="laMiniatura" />
+      </div>
+      <h5 className="card-title">{data.usuarios[0].cursosUsuario[0].nombreCurso}</h5>
+      <p className="card-text">
+       {data.usuarios[0].cursosUsuario[0]}
+      </p>
+      <div className="card-btn-container">
+        <div className="card-btn-fav">
+          <div className="card-btn-stars">
+           {/*  <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar /> */}
+          </div>
+          {/* <div className="flex flex-col items-center text-center font-bold">
+            <BsHeart className="card-btn-heart" />
+            <p>Agregar a tu lista de favoritos</p>
+          </div> */}
+        </div>
+        <div className="card-btn-text">
+          <p>CURSO FREE</p>
+          <Button fontSize={"2xl"} padX={4} padY={2}>
+            <Link to={`/courses/1`} className="flex items-center gap-3">
+              Ver curso <FaAngleRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
         </div>
       </div>
     </div>
