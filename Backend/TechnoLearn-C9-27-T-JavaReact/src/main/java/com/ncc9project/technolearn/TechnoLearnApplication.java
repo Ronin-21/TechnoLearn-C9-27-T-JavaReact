cbf1e5c679dc4f42e545e78d741828fba5f379d5
Alan Cejas <cejasalandavid01@gmail.com>
2023-03-02 18:15:19 -0300

    fix swaggerui

--- Backend/TechnoLearn-C9-27-T-JavaReact/src/main/java/com/ncc9project/technolearn/TechnoLearnApplication.java
+++ Backend/TechnoLearn-C9-27-T-JavaReact/src/main/java/com/ncc9project/technolearn/TechnoLearnApplication.java
@@ -4,11 +4,14 @@ import com.ncc9project.technolearn.Model.Usuario;
 import com.ncc9project.technolearn.Repository.CursosRepository;
 import com.ncc9project.technolearn.Repository.UsuarioRepository;
 import com.ncc9project.technolearn.Service.UsuarioService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class TechnoLearnApplication {
 
