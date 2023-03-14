dc4cade1e346324ce91d3ae2908c6226e207d4ab
Abel Acuña <ale_lasarte@hotmail.com>
2023-03-14 18:30:35 -0300

    Eliminando carpeta Front-end

--- Front-end/src/hook/useModal.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export const useModal = () => {
-	const [isActive, setIsActive] = useState(false);
-
-	const showModal = () => setIsActive(!isActive);
-
-	return [isActive, showModal];
-};
